672072b949bfddac4552cd9a15ccba6362f2f923
Sachin Nadge <nadge.sachin@dhwaniris.com>
2024-06-01 21:22:12 +0530

    remove token

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -22,7 +22,7 @@ router.post("/create",async (req,res)=>{
 })
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
+router.put("/:id",async (req,res)=>{
     try{
        
         const updatedPost=await Post.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
@@ -36,7 +36,7 @@ router.put("/:id",verifyToken,async (req,res)=>{
 
 
 //DELETE
-router.delete("/:id",verifyToken,async (req,res)=>{
+router.delete("/:id",async (req,res)=>{
     try{
         await Post.findByIdAndDelete(req.params.id)
         await Comment.deleteMany({postId:req.params.id})
